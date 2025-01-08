85737cd821412dbb52d9958506b2fdfbcf6e1fe2
0gonge <Challengef0802@gmail.com>
2025-01-08 12:21:41 +0900

    chore: version 수정 (#1)

--- BBANGZIP/Tuist/Package.swift
+++ BBANGZIP/Tuist/Package.swift
@@ -3,20 +3,22 @@
 import PackageDescription
 
 #if TUIST
-    import ProjectDescription
+import ProjectDescription
 
-    let packageSettings = PackageSettings(
-        productTypes: [
-            "Alamofire": .framework,
-            "KakaoOpenSDK": .framework,
-        ]
-    )
+let packageSettings = PackageSettings(
+    productTypes: [
+        "Alamofire": .framework,
+        "KakaoOpenSDK": .framework,
+    ]
+)
 #endif
-let version = "2.23.0"
+let kakaoVersion = "2.23.0"
+let alamofireVersion = "5.10.2"
+
 let package = Package(
     name: "BBANGZIP",
     dependencies: [
-        .package(url: "https://github.com/kakao/kakao-ios-sdk", exact: Version(version)!),
-        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.10.2")
+        .package(url: "https://github.com/kakao/kakao-ios-sdk", exact: Version(kakaoVersion)!),
+        .package(url: "https://github.com/Alamofire/Alamofire", exact: Version(alamofireVersion)!)
     ]
 )
