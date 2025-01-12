63ef1e07ae94a004178ea5a5166bd25712df2cb2
ChoiYouBin <cub8533@naver.com>
2025-01-13 03:33:58 +0900

    feat: OutlinedButton구현 (#5)

---
+++ BBANGZIP/BBANGZIP/Sources/Presentation/OutlinedButton/OutlinedButton.swift
@@ -0,0 +1,48 @@
+//
+//  OutlinedButton.swift
+//  BBANGZIP
+//
+//  Created by 최유빈 on 1/12/25.
+//  Copyright © 2025 com.bbangzip. All rights reserved.
+//
+
+import SwiftUI
+
+struct OutlinedButton: ButtonStyle {
+    
+    var fontType = BbangFont.body2
+    var verticalPadding: CGFloat = 9
+    var customCornerRadius: CGFloat = 16
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .applyFont(font: fontType)
+            .padding(.init(vertical: verticalPadding))
+            .frame(maxWidth: .infinity)
+            .overlay(RoundedRectangle(cornerRadius: customCornerRadius)
+                .stroke(Color("LineStrong"), lineWidth: 1)
+            )
+    }
+}
+
+extension EdgeInsets {
+    var verticalInset: CGFloat { self.top + self.bottom }
+    
+    static func vertical(_ vertical: CGFloat, left: CGFloat = 0, right: CGFloat = 0) -> UIEdgeInsets {
+        UIEdgeInsets(
+            top: vertical,
+            left: left,
+            bottom: vertical,
+            right: right
+        )
+    }
+    
+    init(vertical: CGFloat = 0) {
+        self = EdgeInsets(
+            top: vertical,
+            leading: 0,
+            bottom: vertical,
+            trailing: 0
+        )
+    }
+}
