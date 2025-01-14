a24503e716266d873484b03fb15aa00b11cac4d2
hongseekim <songhee0617@gmail.com>
2025-01-15 00:39:35 +0900

    chore: 변수명 now를 current로 수정 (#13)

--- BBANGZIP/BBANGZIP/Sources/Presentation/Pagination.swift
+++ BBANGZIP/BBANGZIP/Sources/Presentation/Pagination.swift
@@ -10,18 +10,18 @@ import SwiftUI
 
 struct Pagination: View {
     private let total: Int
-    private let now: Int
+    private let current: Int
     
     init(totalPage: Int, nowPage: Int) {
         self.total = totalPage
-        self.now = nowPage
+        self.current = nowPage
     }
     
     var body: some View {
         HStack(spacing: 0) {
             ForEach(1..<total+1, id: \.self) { index in
                 Circle()
-                    .fill(index == now ? Color(.materialDimmer) : Color(.materialDimmer).opacity(0.16))
+                    .fill(index == current ? Color(.materialDimmer) : Color(.materialDimmer).opacity(0.16))
                     .frame(width: 6, height: 6)
                     .padding(.trailing, index == total ? 0 : 8)
             }
