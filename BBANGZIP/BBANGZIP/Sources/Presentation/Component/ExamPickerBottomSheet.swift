d10d11c4af81a704274021369dbcdd5d095943af
0gonge <Challengef0802@gmail.com>
2025-01-17 03:24:23 +0900

    refactor: ExamplePickerBottomSheet 변수화 (#25)

--- BBANGZIP/BBANGZIP/Sources/Presentation/Component/ExamPickerBottomSheet.swift
+++ BBANGZIP/BBANGZIP/Sources/Presentation/Component/ExamPickerBottomSheet.swift
@@ -55,83 +55,97 @@ struct ExamPickerBottomSheet: View {
     
     var body: some View {
         VStack(spacing: 16) {
-            CustomText("시험이 언제인가요?",
-                       fontType: .headline1Medium,
-                       color: (BBANGZIPAsset.Assets.labelNeutral.swiftUIColor)
-            )
-            HStack(spacing: 0) {
-                Picker(
-                    selection: $selectedYear,
-                    label: Text("")
-                ) {
-                    ForEach(
-                        years.filter {
-                            $0 >= currentYear
-                        },
-                        id: \.self) { year in
-                            CustomText("\(year)년",
-                                       fontType: .heading2Bold,
-                                       color: (BBANGZIPAsset.Assets.labelStrong.swiftUIColor))
-                            .tag(year)
-                        }
-                }
-                .pickerStyle(WheelPickerStyle())
-                .padding(.trailing, -25)
-                .clipped()
-                .onChange(of: selectedYear) { _ in
-                    updateSelectedDay()
-                }
-                
-                Picker(
-                    selection: $selectedMonth,
-                    label: Text("")
-                ) {
-                    ForEach(
-                        validMonths,
-                        id: \.self
-                    ) { month in
-                        CustomText("\(month)월",
-                                   fontType: .heading2Bold,
-                                   color: (BBANGZIPAsset.Assets.labelStrong.swiftUIColor))
-                        .tag(month)
-                    }
-                }
-                .pickerStyle(WheelPickerStyle())
-                .padding(.leading, -25)
-                .padding(.trailing, -25)
-                .clipped()
-                .onChange(of: selectedMonth) { _ in
-                    updateSelectedDay()
-                }
-                
-                Picker(
-                    selection: $selectedDay,
-                    label: Text("")
-                ) {
-                    ForEach(days.filter { isValidDay($0) }, id: \.self) { day in
-                        CustomText("\(day)일",
-                                   fontType: .heading2Bold,
-                                   color: (BBANGZIPAsset.Assets.labelStrong.swiftUIColor))
-                        .tag(day)
-                    }
-                }
-                .pickerStyle(WheelPickerStyle())
-                .padding(.leading, -25)
-                .clipped()
-            }
-            .padding(.horizontal, 20)
-            
-            Button(action: {
-                withAnimation {
-                    isPresented = false
-                }
-            }) {
-                Text("시험 일자 입력하기")
-            }
-            .buttonStyle(SolidButton())
-            .padding(.horizontal)
+            headerView
+            pickersView
+            actionButton
         }
         .padding()
+    }
+    
+    private var headerView: some View {
+        CustomText(
+            "시험이 언제인가요?",
+            fontType: .headline1Medium,
+            color: (BBANGZIPAsset.Assets.labelNeutral.swiftUIColor)
+        )
+    }
+    
+    private var pickersView: some View {
+        HStack(spacing: 0) {
+            yearPicker
+            monthPicker
+            dayPicker
+        }
+        .padding(.horizontal, 20)
+    }
+    
+    private var yearPicker: some View {
+        Picker(selection: $selectedYear, label: Text("")) {
+            ForEach(
+                years.filter {
+                    $0 >= currentYear
+                },
+                id: \.self) { year in
+                CustomText("\(year)년",
+                           fontType: .heading2Bold,
+                           color: (BBANGZIPAsset.Assets.labelStrong.swiftUIColor))
+                .tag(year)
+            }
+        }
+        .pickerStyle(WheelPickerStyle())
+        .padding(.trailing, -25)
+        .clipped()
+        .onChange(
+            of: selectedYear
+        ) {
+            _ in updateSelectedDay()
+        }
+    }
+    
+    private var monthPicker: some View {
+        Picker(selection: $selectedMonth, label: Text("")) {
+            ForEach(validMonths, id: \.self) { month in
+                CustomText("\(month)월",
+                           fontType: .heading2Bold,
+                           color: (BBANGZIPAsset.Assets.labelStrong.swiftUIColor))
+                .tag(month)
+            }
+        }
+        .pickerStyle(WheelPickerStyle())
+        .padding(.leading, -25)
+        .padding(.trailing, -25)
+        .clipped()
+        .onChange(
+            of: selectedMonth
+        ) {
+            _ in updateSelectedDay()
+        }
+    }
+    
+    private var dayPicker: some View {
+        Picker(selection: $selectedDay, label: Text("")) {
+            ForEach(days.filter { isValidDay($0) }, id: \.self) { day in
+                CustomText("\(day)일",
+                           fontType: .heading2Bold,
+                           color: (BBANGZIPAsset.Assets.labelStrong.swiftUIColor))
+                .tag(day)
+            }
+        }
+        .pickerStyle(WheelPickerStyle())
+        .padding(.leading, -25)
+        .clipped()
+    }
+    
+    private var actionButton: some View {
+        Button(action: {
+            withAnimation {
+                isPresented = false
+            }
+        }) {
+            Text("시험 일자 입력하기")
+        }
+        .buttonStyle(SolidButton())
+        .padding(.horizontal)
     }
     
     private var validMonths: [Int] {
