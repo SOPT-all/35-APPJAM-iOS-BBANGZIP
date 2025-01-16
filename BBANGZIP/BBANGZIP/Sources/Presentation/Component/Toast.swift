381af5ff588fd044f9ebb2b09a49770c405222bf
hongseekim <songhee0617@gmail.com>
2025-01-16 17:04:53 +0900

    feat: Toast 컴포넌트 구현 (#31)

---
+++ BBANGZIP/BBANGZIP/Sources/Presentation/Component/Toast.swift
@@ -0,0 +1,59 @@
+//
+//  Toast.swift
+//  BBANGZIP
+//
+//  Created by 김송희 on 1/16/25.
+//  Copyright © 2025 com.bbangzip. All rights reserved.
+//
+
+import SwiftUI
+
+struct Toast: Equatable {
+    var firstLineText: String
+    var secondLineText: String?
+    var type: ToastType
+    var startFrom: CGFloat
+    
+    init ( _ firstLineText: String,
+           secondLineText: String? = nil,
+           type: ToastType = .singleLine,
+           startFrom: CGFloat = 16
+    ) {
+        self.firstLineText = firstLineText
+        self.secondLineText = secondLineText
+        self.type = type
+        self.startFrom = startFrom
+    }
+}
+
+struct ToastView: View {
+    private let toast: Toast
+    
+    init(toast: Toast) {
+        self.toast = toast
+    }
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            CustomText(
+                toast.firstLineText,
+                fontType: .label2Bold,
+                color: Color(.staticWhite)
+            )
+            
+            if let secondLineText  = toast.secondLineText {
+                CustomText(
+                    secondLineText,
+                    fontType: .label2Bold,
+                    color: Color(.staticWhite)
+                )
+            }
+        }
+        .padding(.horizontal, toast.type.horizontalInset)
+        .padding(.vertical, toast.type.verticalInset)
+        .background(
+            RoundedRectangle(cornerRadius: toast.type.radius)
+                .fill(Color(.labelAlternative))
+        )
+    }
+}
