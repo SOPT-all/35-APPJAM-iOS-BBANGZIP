27d8bc37cd5ede532ff36a910fa5412d97e4f7ac
ChoiYouBin <cub8533@naver.com>
2025-01-17 09:48:51 +0900

    feat: CustomTabView 구현을 위한 구조 변경 (#34)

--- BBANGZIP/BBANGZIP/Sources/Presentation/Main/View/CustomTabView.swift
+++ BBANGZIP/BBANGZIP/Sources/Presentation/Main/View/CustomTabView.swift
@@ -9,57 +9,46 @@
 import SwiftUI
 
 struct CustomTabView: View {
+    enum Tab {
+        case subjectManage
+        case todo
+        case networking
+        case mypage
+    }
+    
+    @State private var selected: Tab = .subjectManage
+    
     var body: some View {
-        TabView {
-            Text("과목 관리")
-                .tabItem {
-                    Image(.subjectManage)
-                        .renderingMode(.template)
-                        .foregroundStyle(Color(.labelAssistive))
-                    CustomText(
-                        "과목 관리",
-                        fontType: .caption1Bold,
-                        color: Color(.labelAssistive)
-                    )
-                }
-            Text("오늘 할 일")
-                .tabItem {
-                    Image(.todo)
-                        .renderingMode(.template).foregroundStyle(Color(.labelAssistive))
-                    CustomText(
-                        "오늘 할 일",
-                        fontType: .caption1Bold,
-                        color: Color(.labelAssistive)
-                    )
-                }
-            Text("이웃 목록")
-                .tabItem {
-                    Image(.networking)
-                        .renderingMode(.template).foregroundStyle(Color(.labelAssistive))
-                    CustomText(
-                        "이웃 목록",
-                        fontType: .caption1Bold,
-                        color: Color(.labelAssistive)
-                    )
-                }
-            Text("마이페이지")
-                .tabItem {
-                    Image(.myPage)
-                        .renderingMode(.template).foregroundStyle(Color(.labelAssistive))
-                    CustomText(
-                        "마이페이지",
-                        fontType: .caption1Bold,
-                        color: Color(.labelAssistive)
-                    )
-                }
+        TabView(selection: $selected) {
+            Group {
+                Text("과목 관리")
+                    .tag(Tab.subjectManage)
+                
+                Text("오늘 할 일")
+                    .tag(Tab.todo)
+                
+                Text("이웃 목록")
+                    .tag(Tab.networking)
+                
+                Text("마이페이지")
+                    .tag(Tab.mypage)
+            }
+            .toolbar(.hidden, for: .tabBar)
         }
-        .onAppear() {
-            UITabBar.appearance().barTintColor = UIColor(Color(.backgroundNormal))
-        }
-        .accentColor(Color(.labelNormal))
     }
 }
 
 #Preview {
     CustomTabView()
 }
+
+//.tabItem {
+//    Image(.subjectManage)
+//        .renderingMode(.template)
+//        .foregroundStyle(Color(.labelAssistive))
+//    CustomText(
+//        "과목 관리",
+//        fontType: .caption1Bold,
+//        color: Color(.labelAssistive)
+//    )
+//}
