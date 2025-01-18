458024a1adb5ad3b6d5e6f1d36ed26ced3cd6354
0gonge <Challengef0802@gmail.com>
2025-01-19 05:31:37 +0900

    feat: KeyChainManager 구현 (#37)

--- BBANGZIP/BBANGZIP/Sources/Data/Keychain/KeychainManager.swift
+++ BBANGZIP/BBANGZIP/Sources/Data/Keychain/KeychainManager.swift
@@ -7,3 +7,91 @@
 //
 
 import Foundation
+import Security
+
+protocol KeyChainManager: Sendable {
+    func saveValue(
+        token: String,
+        type: TokenType
+    ) -> OSStatus
+    func changeValue(token: String) -> OSStatus
+    func searchValue() -> String?
+    @discardableResult
+    func removeValue() -> OSStatus
+}
+
+final class DefaultKeyChainManager: KeyChainManager {
+    private let serviceName: String = "com.bbangzip.io.tuist.BBANGZIP"
+    
+    func saveValue(
+        token: String,
+        type: TokenType
+    ) -> OSStatus {
+        let saveData: CFDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: serviceName,
+            kSecReturnData: true,
+            kSecValueData: token.data(using: .utf8)!
+        ] as CFDictionary
+        let status = SecItemAdd(saveData, nil)
+        handleStatus(status: status)
+        return status
+    }
+    
+    func changeValue(token: String) -> OSStatus {
+        let savedData: CFDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: serviceName
+        ] as CFDictionary
+        
+        let updateData: CFDictionary = [
+            kSecValueData: token.data(using: .utf8)!
+        ] as CFDictionary
+        return SecItemUpdate(
+            savedData,
+            updateData
+        )
+    }
+    
+    func searchValue() -> String? {
+        let savedData: CFDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: serviceName,
+            kSecReturnData: true
+        ] as CFDictionary
+        var searchWord:CFTypeRef? = nil
+        let searchResult = SecItemCopyMatching(
+            savedData,
+            &searchWord
+        )
+        if searchResult != errSecSuccess {
+            return nil
+        }
+        let searchData: Data = searchWord as! Data
+        return String(
+            data: searchData,
+            encoding: .utf8
+        )
+    }
+    
+    @discardableResult
+    func removeValue() -> OSStatus {
+        let savedData: CFDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: serviceName,
+            kSecReturnData: true
+        ] as CFDictionary
+        return SecItemDelete(savedData)
+    }
+    
+    private func handleStatus(status: OSStatus) {
+        switch status {
+        case errSecSuccess:
+            break
+        case errSecDuplicateItem:
+            print("duplicated")
+        default:
+            print("default")
+        }
+    }
+}
