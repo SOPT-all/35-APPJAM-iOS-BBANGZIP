725a22fc460c30647baebc4c3243dc1c269030fc
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-01-18 23:52:11 +0900

    feat: Subject Card 구현 (#43)

--- BBANGZIP/BBANGZIP/Sources/Presentation/Component/Card/SubjectCard/SubjectCard.swift
+++ BBANGZIP/BBANGZIP/Sources/Presentation/Component/Card/SubjectCard/SubjectCard.swift
@@ -9,11 +9,133 @@
 import SwiftUI
 
 struct SubjectCard: View {
+    @State private var state: CardState
+    // TODO: API 연결 후 주입 값 형식 변경, 현재는 필요한 값을 struct로 묶어 주입하는 형식
+    private let subjectCardData: SubjectCardData
+    
+    init(
+        state: CardState,
+        subjectCardData: SubjectCardData
+    ) {
+        self.state = state
+        self.subjectCardData = subjectCardData
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            backgroundView
+            
+            HStack(alignment: .center) {
+                VStack(
+                    alignment: .leading,
+                    spacing: 4
+                ) {
+                    CustomText(
+                        subjectCardData.title,
+                        fontType: .body1Bold,
+                        color: Color(.labelNormal)
+                    )
+                    
+                    CustomText(
+                        subjectCardData.test,
+                        fontType: .label2Bold,
+                        color: Color(.labelNeutral)
+                    )
+                    
+                    Chip(type: subjectCardData.chipType)
+                    
+                    Spacer()
+                    
+                    delayedStudyView
+                    
+                    inProgressStudyView
+                }
+                .padding(
+                    .vertical,
+                    16
+                )
+                .padding(
+                    .leading,
+                    16
+                )
+                
+                Spacer()
+                
+                Image(.chevronRight)
+                    .renderingMode(.template)
+                    .foregroundStyle(Color(.labelAssistive))
+                    .padding(
+                        .trailing,
+                        6
+                    )
+            }
+            
+        }
+        .frame(
+            width: 158,
+            height: 190
+        )
+    }
+    
+    private var backgroundView: some View {
+        RoundedRectangle(cornerRadius: 24)
+            .fill(state.backgroundColor)
+            .overlay(
+                RoundedRectangle(cornerRadius: 24)
+                    .stroke(
+                        state.borderColor,
+                        lineWidth: 3
+                    )
+            )
+    }
+    
+    private var delayedStudyView: some View {
+        HStack(spacing: 4) {
+            PushIcon(
+                number: subjectCardData.delayedStudyCount,
+                type: .orange
+            )
+            
+            CustomText(
+                "밀린 공부",
+                fontType: .caption1Bold,
+                color: Color(.labelAssistive)
+            )
+        }
+    }
+    
+    private var inProgressStudyView: some View {
+        HStack(spacing: 4) {
+            PushIcon(
+                number: subjectCardData.inProgressStudyCount,
+                type: .black
+            )
+            
+            CustomText(
+                "진행 중인 공부",
+                fontType: .caption1Bold,
+                color: Color(.labelAssistive)
+            )
+        }
     }
 }
 
 #Preview {
-    SubjectCard()
+    ZStack {
+        Color(.systemPink)
+        VStack(spacing: 10) {
+            SubjectCard(
+                state: SubjectCardState.cardDefault,
+                subjectCardData: .mockData
+            )
+            SubjectCard(
+                state: SubjectCardState.selected,
+                subjectCardData: .mockData
+            )
+            SubjectCard(
+                state: SubjectCardState.selectable,
+                subjectCardData: .mockData
+            )
+        }
+    }
 }
