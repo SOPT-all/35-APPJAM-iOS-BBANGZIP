19c52792adf9719f77fda3d81709f818b90ca2fb
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-01-20 22:19:50 +0900

    refactor: TextField 로직 반영 (#67)

--- BBANGZIP/BBANGZIP/Sources/Presentation/Main/ViewModel/NameInputViewModel.swift
+++ BBANGZIP/BBANGZIP/Sources/Presentation/Main/ViewModel/NameInputViewModel.swift
@@ -27,47 +27,4 @@ final class NameInputViewModel: ObservableObject {
         self.announceState = announceState
         self.state = state
     }
-    
-    private func validateNickname(
-        oldText: String,
-        newText: String
-    ) {
-        if let maxLength = TextFieldStyleCase.nickname.maxLength {
-            if newText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
-                // TODO: 공백으로만 이루어진 경우 로직 추가 필요
-            }
-            if newText.count > maxLength {
-                nickname = String(newText.prefix(maxLength))
-                state = .alert
-                announceState = .maxLengthWrong
-            } else {
-                var result = newText
-                for char in oldText {
-                    if let index = result.firstIndex(of: char) {
-                        result.remove(at: index)
-                    }
-                }
-                
-                if result.isEmpty {
-                    // TODO: 공백인 경우 로직 추가 필요
-                } else {
-                    if result.containsEmoji {
-                        state = .alert
-                        announceState = .emojiWrong
-                    } else if result.containsSymbol {
-                        state = .alert
-                        announceState = .symbolWrong
-                    } else {
-                        if newText.isEmpty {
-                            state = .placeholder
-                            announceState = .defaultCorrect
-                        } else {
-                            state = .complete
-                            announceState = .completeCorrect
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
