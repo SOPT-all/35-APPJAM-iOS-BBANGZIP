a1788fbbe836570e308bed39f6026845c946cd8d
ChoiYouBin <cub8533@naver.com>
2025-01-21 06:22:08 +0900

    feat: menuTab 추가 (#73)

--- BBANGZIP/BBANGZIP/Sources/Presentation/SubjectDetail/SubjectDetailView.swift
+++ BBANGZIP/BBANGZIP/Sources/Presentation/SubjectDetail/SubjectDetailView.swift
@@ -11,27 +11,45 @@ import SwiftUI
 struct SubjectDetailView: View {
     var body: some View {
         VStack {
-            HStack {
-                CustomText(
-                    "사장님의 각오 한 마디를 작성해 보세요",
-                    fontType: .heading2Bold,
-                    color: Color(.labelAssistive)
-                )
-                .lineLimit(2)
-                .padding(.leading, 24)
-                .padding(.trailing, 151)
+            ZStack {
+                VStack {
+                    HStack {
+                        CustomText(
+                            "사장님의 각오 한 마디를 작성해 보세요",
+                            fontType: .heading2Bold,
+                            color: Color(.labelAssistive)
+                        )
+                        .lineLimit(2)
+                        .padding(.leading, 24)
+                        .padding(.trailing, 151)
+                        
+                        Spacer()
+                    }
+                    .padding(.top, 25)
+                    .padding(.bottom, 72)
+                    .background(
+                        RoundedRectangle(cornerRadius: 32)
+                            .fill(Color(.backgroundAccent))
+                            .edgesIgnoringSafeArea(.top)
+                    )
+                    
+                    Spacer()
+                }
                 
-                Spacer()
+                VStack {
+                    MenuTab(
+                        tabNames: [
+                            "중간고사",
+                            "기말고사"
+                        ]
+                    )
+                    .padding(.horizontal, 20)
+                    .padding(.top, 125)
+                    
+                    Spacer()
+                }
+                
             }
-            .padding(.top, 25)
-            .padding(.bottom, 72)
-            .background(
-                RoundedRectangle(cornerRadius: 32)
-                    .fill(Color(.backgroundAccent))
-                    .edgesIgnoringSafeArea(.top)
-            )
-            
-            Spacer()
         }
     }
 }
