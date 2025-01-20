8043c23b63c089d3c47537b29158b58f17b90415
hongseekim <songhee0617@gmail.com>
2025-01-20 23:32:50 +0900

    chore: extension 폴더 분리 (#66)

--- BBANGZIP/BBANGZIP/Sources/Presentation/Main/View/OnboardingView.swift
+++ BBANGZIP/BBANGZIP/Sources/Presentation/Main/View/OnboardingView.swift
@@ -8,18 +8,6 @@
 
 import SwiftUI
 
-extension View {
-    func hideKeyboard() {
-        let resign = #selector(UIResponder.resignFirstResponder)
-        UIApplication.shared.sendAction(
-            resign,
-            to: nil,
-            from: nil,
-            for: nil
-        )
-    }
-}
-
 enum OnboardingButtonText {
     case start
     case inProgress
