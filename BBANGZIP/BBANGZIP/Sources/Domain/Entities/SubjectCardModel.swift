121da3dfed90c94d67b3c6a9d50912e6ea820653
ChoiYouBin <cub8533@naver.com>
2025-01-21 03:20:03 +0900

    feat: SubjectCardModel 추가 (#46)

---
+++ BBANGZIP/BBANGZIP/Sources/Domain/Entities/SubjectCardModel.swift
@@ -0,0 +1,62 @@
+//
+//  SubjectCardModel.swift
+//  BBANGZIP
+//
+//  Created by 최유빈 on 1/21/25.
+//  Copyright © 2025 com.bbangzip. All rights reserved.
+//
+
+
+struct SubjectCardModel: Sendable {
+    var state: CardState
+    let subjectId: Int
+    let subjectName: String
+    let studyList: StudyListModel
+}
+
+struct StudyListModel: Sendable {
+    let examName: String
+    let examDday: Int
+    let pendingCount: Int
+    let inProgressCount: Int
+}
+
+extension SubjectCardModel {
+    static let mockList: [Self] = [
+        .init(
+            state: SubjectCardState.cardDefault,
+            subjectId: 1,
+            subjectName: "경제통계학",
+            studyList: StudyListModel(
+                examName: "중간고사",
+                examDday: 14,
+                pendingCount: 2,
+                inProgressCount: 1
+            )
+        ),
+        .init(
+            state: SubjectCardState.cardDefault,
+            subjectId: 2,
+            subjectName: "컴퓨터프로그래밍1",
+            studyList: StudyListModel(
+                examName: "중간고사",
+                examDday: 20,
+                pendingCount: 1,
+                inProgressCount: 4
+            )
+        ),
+        .init(
+            state: SubjectCardState.cardDefault,
+            subjectId: 3,
+            subjectName: "한국사와문학",
+            studyList: StudyListModel(
+                examName: "기말고사",
+                examDday: 3,
+                pendingCount: 0,
+                inProgressCount: 0
+            )
+        )
+
+
+    ]
+}
