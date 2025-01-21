8db84cc52b8ea0c34f8bbe1d17c41f47b96cc154
ChoiYouBin <cub8533@naver.com>
2025-01-21 15:32:24 +0900

    feat: 동기 부여 메시지, 시험 선택, 날짜 섹션 구현 (#73)

--- BBANGZIP/BBANGZIP/Sources/Presentation/SubjectDetail/SubjectDetailView.swift
+++ BBANGZIP/BBANGZIP/Sources/Presentation/SubjectDetail/SubjectDetailView.swift
@@ -9,48 +9,72 @@
 import SwiftUI
 
 struct SubjectDetailView: View {
+    
     var body: some View {
-        VStack {
-            ZStack {
-                VStack {
-                    HStack {
-                        CustomText(
-                            "사장님의 각오 한 마디를 작성해 보세요",
-                            fontType: .heading2Bold,
-                            color: Color(.labelAssistive)
-                        )
-                        .lineLimit(2)
-                        .padding(.leading, 24)
-                        .padding(.trailing, 151)
+        ScrollView {
+            VStack {
+                ZStack {
+                    VStack {
+                        backgroundView
                         
                         Spacer()
                     }
-                    .padding(.top, 25)
-                    .padding(.bottom, 72)
-                    .background(
-                        RoundedRectangle(cornerRadius: 32)
-                            .fill(Color(.backgroundAccent))
-                            .edgesIgnoringSafeArea(.top)
-                    )
                     
-                    Spacer()
+                    VStack(spacing: 16) {
+                        MenuTab(
+                            tabNames: [
+                                "중간고사",
+                                "기말고사"
+                            ]
+                        )
+                        .padding(.top, 125)
+                        .padding(.horizontal, 20)
+                        
+                        HStack(spacing: 8) {
+                            Chip(type: .daysLeftWithText(24))
+                            
+                            CustomText(
+                                "2025년 5월 13일",
+                                fontType: .label1Bold,
+                                color: Color(.labelAlternative)
+                            )
+                        }
+                        
+                        Spacer()
+                    }
                 }
-                
-                VStack {
-                    MenuTab(
-                        tabNames: [
-                            "중간고사",
-                            "기말고사"
-                        ]
-                    )
-                    .padding(.horizontal, 20)
-                    .padding(.top, 125)
-                    
-                    Spacer()
-                }
-                
             }
         }
+        .navigationBarBackground({
+            Color(.backgroundAccent)
+        })
+    }
+    
+    var backgroundView: some View {
+        HStack {
+            CustomText(
+                "사장님의 각오 한 마디를 작성해 보세요",
+                fontType: .heading2Bold,
+                color: Color(.labelAssistive)
+            )
+            .lineLimit(2)
+            .padding(.leading, 24)
+            .padding(.trailing, 151)
+            
+            Spacer()
+        }
+        .padding(.top, 25)
+        .padding(.bottom, 72)
+        .background(
+            Color(.backgroundAccent)
+                .cornerRadius(
+                    32,
+                    corners: [
+                        .bottomLeft,
+                        .bottomRight
+                    ]
+                )
+        )
     }
 }
 
