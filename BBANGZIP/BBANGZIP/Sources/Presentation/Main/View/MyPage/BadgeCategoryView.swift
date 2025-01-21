903a7624cb89440edabc4e1bbe6716adaaee33b3
0gonge <Challengef0802@gmail.com>
2025-01-22 02:27:38 +0900

    feat: CustomNavigation 연결 (#79)

---
+++ BBANGZIP/BBANGZIP/Sources/Presentation/Main/View/MyPage/BadgeCategoryView.swift
@@ -0,0 +1,17 @@
+//
+//  BadgeCategoryView.swift
+//  BBANGZIP
+//
+//  Created by 송여경 on 1/22/25.
+//  Copyright © 2025 com.bbangzip. All rights reserved.
+//
+
+import SwiftUI
+
+struct BadgeCategoryView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
